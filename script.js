a4c0241a5c5312e71377a395366e2946c4425264
Vitor Hermano <vhermano78@gmail.com>
2024-10-29 21:11:48 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,131 @@
+// Scroll Animations //
+
+var tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.sectionTwo',
+        start: '0% 95%',
+        end: '70% 50%',
+        scrub: true,
+        markers: true,
+    },
+});
+
+tl.to(
+    '#fanta',
+    {
+        top: '120%',
+        left: '0%',
+    },
+    'orange'
+);
+
+tl.to(
+    '#cut-orange',
+    {
+        top: '160%',
+        left: '23%',
+    },
+    'orange'
+);
+
+tl.to(
+    '#orange',
+    {
+        width: '15%',
+        top: '160%',
+        right: '10%',
+    },
+    'orange'
+);
+
+tl.to(
+    '#leaf',
+    {
+        top: '110%',
+        rotate: '600deg',
+        left: '70%',
+    },
+    'orange'
+);
+
+tl.to(
+    '#leaf2',
+    {
+        top: '110%',
+        rotate: '530deg',
+        left: '0%',
+    },
+    'orange'
+);
+
+// Second Timeline //
+
+var tl2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.sectionThree',
+        start: '0% 95%',
+        end: '20% 50%',
+        scrub: true,
+        markers: true,
+    },
+});
+
+tl2.from(
+    '.lemon1',
+    {
+        rotate: '-90deg',
+        left: '-100%',
+        top: '110%',
+    },
+    'ca'
+);
+
+tl2.from(
+    '#cocacola',
+    {
+        rotate: '-90deg',
+        left: '-100%',
+        top: '110%',
+    },
+    'ca'
+);
+
+tl2.from(
+    '.lemon2',
+    {
+        rotate: '90deg',
+        left: '100%',
+        top: '110%',
+    },
+    'ca'
+);
+
+tl2.from(
+    '#pepsi',
+    {
+        rotate: '90deg',
+        left: '100%',
+        top: '110%',
+    },
+    'ca'
+);
+
+tl2.to(
+    '#cut-orange',
+    {
+        width: '18%',
+        left: '42%',
+        top: '204%',
+    },
+    'ca'
+);
+
+tl2.to(
+    '#fanta',
+    {
+        width: '35%',
+        left: '33%',
+        top: '213%',
+    },
+    'ca'
+);
